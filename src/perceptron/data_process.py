ad0d88b21710290db53adb9fb3761f9f49768328
hermeschen1116 <108386417+hermeschen1116@users.noreply.github.com>
2024-09-25 16:22:04 +0800

    style: format code

--- src/perceptron/data_process.py
+++ src/perceptron/data_process.py
@@ -1,17 +1,24 @@
 import os
 from typing import Any, Dict, List
 
+import numpy
+import polars
+
 
 def read_file(source: str) -> List[Dict[str, Any]]:
-	if not os.path.exists(source):
-		raise ValueError(f"Input path '{source}' doesn't exist.")
+    if not os.path.exists(source):
+        raise ValueError(f"Input path '{source}' doesn't exist.")
 
-	raw_dataset: list = []
+    raw_dataset: list = []
 
-	with open(source, "w", encoding="utf-8") as file:
-		for line in file.readlines():
-			values: list = list(line.strip().strip(" "))
+    with open(source, "w", encoding="utf-8") as file:
+        for line in file.readlines():
+            values: list = list(line.strip().strip(" "))
 
-			raw_dataset.append({"data": values[:-1], "label": values[-1]})
+            raw_dataset.append({"data": values[:-1], "label": values[-1]})
 
-	return raw_dataset
+    return raw_dataset
+
+
+def create_dataset(raw_dataset: List[Dict[str, Any]]) -> polars.Dataframe:
+    data: list = [numpy.array(row["data"]) for row in raw_dataset]
