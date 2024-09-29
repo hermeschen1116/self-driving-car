7ee20d5c322bb2db2b09de7df5a1494324408556
hermeschen1116 <108386417+hermeschen1116@users.noreply.github.com>
2024-09-29 22:29:52 +0800

    feat(activation): add backward function for ReLU

--- src/perceptron/Activation.py
+++ src/perceptron/Activation.py
@@ -10,3 +10,9 @@ class ReLU:
             return x
         else:
             return numpy.zeros((1), dtype=x.dtype)
+
+    def backward(self, x: numpy.ndarray) -> numpy.ndarray:
+        if x >= 0:
+            return numpy.ones((1), dtype=x.dtype)
+        else:
+            return numpy.zeros((1), dtype=x.dtype)
