f35c35047aac263b599c726eb8e454efb682d641
hermeschen1116 <108386417+hermeschen1116@users.noreply.github.com>
2024-09-29 21:59:55 +0800

    chore(activation): remove ReLU

--- src/perceptron/Activation.py
+++ src/perceptron/Activation.py
@@ -1,12 +1,1 @@
 import numpy
-
-
-class ReLU:
-    def __init__(self) -> None:
-        pass
-
-    def forward(self, x: numpy.ndarray) -> numpy.ndarray:
-        if x >= 0:
-            return x
-        else:
-            return numpy.zeros((1))
