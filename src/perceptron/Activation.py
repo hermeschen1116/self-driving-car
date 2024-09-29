95845114b53d6aca3884c137b72fd8f578b0d3df
hermeschen1116 <108386417+hermeschen1116@users.noreply.github.com>
2024-09-29 22:02:44 +0800

    feat(activation): implement UnitStep activation

--- src/perceptron/Activation.py
+++ src/perceptron/Activation.py
@@ -1,1 +1,12 @@
 import numpy
+
+
+class UnitStep:
+    def __init__(self) -> None:
+        pass
+
+    def forwared(self, x: numpy.ndarray) -> numpy.ndarray:
+        if x > 0:
+            return numpy.ones((1))
+        else:
+            return numpy.zeros((1))
