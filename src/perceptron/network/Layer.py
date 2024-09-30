1e64e9a49030b3d2cd38ff801534914cf4614d3f
hermeschen1116 <108386417+hermeschen1116@users.noreply.github.com>
2024-09-30 17:15:55 +0800

    fix(layer): remove arg to set bias

--- src/perceptron/network/Layer.py
+++ src/perceptron/network/Layer.py
@@ -10,21 +10,18 @@ class Linear(Module):
 		self,
 		input_features: int,
 		output_features: int,
-		bias: bool,
 		dtype: Optional[type] = numpy.float32,
 	) -> None:
 		super(Linear, self).__init__()
 		self.weights: numpy.ndarray = numpy.random.rand((input_features + 1), output_features)
 
-		self.bias: bool = bias
-
 		self.dtype: type = dtype
 
 	def forward(self, x: numpy.ndarray) -> numpy.ndarray:
 		batch_size: int = x.shape[0]
-		bias_array: numpy.ndarray = numpy.ones((batch_size, 1), dtype=self.dtype)
+		bias: numpy.ndarray = numpy.ones((batch_size, 1), dtype=self.dtype)
 
-		x_with_bias: numpy.ndarray = numpy.concatenate((x, bias_array), dtype=self.dtype)
+		x_with_bias: numpy.ndarray = numpy.concatenate((x, bias), dtype=self.dtype)
 
 		return x_with_bias.dot(self.weights)
 
