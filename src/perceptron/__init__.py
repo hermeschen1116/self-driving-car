e6baae2f189e0ded24845da3cb90cbe3b0507399
hermeschen1116 <108386417+hermeschen1116@users.noreply.github.com>
2024-09-30 10:00:13 +0800

    feat: create abstract class for all module

--- src/perceptron/__init__.py
+++ src/perceptron/__init__.py
@@ -1,2 +1,21 @@
+from abc import ABC, abstractmethod
+
+import numpy
+
+
 def hello() -> None:
 	print("Hello from perceptron!")
+
+class Module(ABC):
+	def __init__(self) -> None:
+		super().__init__()
+
+	@classmethod
+	@abstractmethod
+	def forward(cls, x: numpy.ndarray) -> numpy.ndarray:
+		return x
+
+	@classmethod
+	@abstractmethod
+	def backward(cls, y_i: numpy.ndarray) -> numpy.ndarray:
+		return y_i
