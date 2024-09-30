83f8ebb68032186243eed59cd82a26c6736a63bc
hermeschen1116 <108386417+hermeschen1116@users.noreply.github.com>
2024-09-30 21:23:42 +0800

    feat(model): implement optimize

--- src/perceptron/network/Model.py
+++ src/perceptron/network/Model.py
@@ -13,22 +13,24 @@ class Perceptron(Module):
 		self,
 		input_features: int,
 		output_features: int,
+		learnin_rate: float,
 		num_hidden_layers: int = 1,
-		dtype: Optional[type] = numpy.float32,
+		dtype: Optional[type] = numpy.float32
 	) -> None:
 		super(Perceptron, self).__init__()
 
 		hidden_layer_features: int = random.randint(input_features, output_features)
 
 		self.layers: list = [
-			{"input_layer": Linear(input_features, hidden_layer_features, dtype)},
-			{"input_activation": ReLU()},
-			{"hidden_layer": Linear(hidden_layer_features, hidden_layer_features, dtype)},
-			{"hidden_activation": ReLU},
-			{"output_layer": Linear(hidden_layer_features, output_features, dtype)},
-			{"output_activation": ReLU()},
+			Linear(input_features, hidden_layer_features, dtype),
+			ReLU(),
+			Linear(hidden_layer_features, hidden_layer_features, dtype),
+			ReLU,
+			Linear(hidden_layer_features, output_features, dtype),
+			ReLU()
 		]
 
+		self.learning_rate: float = learnin_rate
 		self.dtype: type = dtype
 
 	def forward(self, x: numpy.ndarray) -> numpy.ndarray:
@@ -36,8 +38,21 @@ class Perceptron(Module):
 
 		y_i: numpy.ndarray = x_i
 		for layer in self.layers:
-			y_i = layer.values()[0](x_i)
+			y_i = layer(x_i)
 			if layer is not self.layers[-1]:
 				x_i = y_i
 
 		return y_i
+
+	def optimize(self, loss_gradient: numpy.ndarray) -> None:
+		local_gradient: numpy.ndarray = loss_gradient * self.layers[-1].gradient
+
+		self.layers[-2] -= self.learning_rate * local_gradient * self.layers[-2].gradient
+
+		local_gradient *= self.layers[-2].weights * self.layers[-3].gradient
+
+		self.layers[-4] -= self.learning_rate * local_gradient * self.layers[-4].gradient
+
+		local_gradient *= self.layers[-4].weights * self.layers[-5].gradient
+
+		self.layers[-6] -= self.learning_rate * local_gradient * self.layers[-6].gradient
