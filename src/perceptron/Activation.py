17ac9c2ddad1e6c4fbcf437ee66c466915774361
hermeschen1116 <108386417+hermeschen1116@users.noreply.github.com>
2024-09-30 09:47:15 +0800

    style(activation): rewrite function in math style

--- src/perceptron/Activation.py
+++ src/perceptron/Activation.py
@@ -11,8 +11,8 @@ class ReLU:
 		else:
 			return numpy.zeros((1), dtype=x.dtype)
 
-	def backward(self, x: numpy.ndarray) -> numpy.ndarray:
-		if x >= 0:
-			return numpy.ones((1), dtype=x.dtype)
+	def backward(self, y_i: numpy.ndarray) -> numpy.ndarray:
+		if y_i >= 0:
+			return numpy.ones((1), dtype=y_i.dtype)
 		else:
-			return numpy.zeros((1), dtype=x.dtype)
+			return numpy.zeros((1), dtype=y_i.dtype)
