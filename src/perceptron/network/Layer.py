a757d7040fba53bddfa705203065776a7c8f8aec
hermeschen1116 <108386417+hermeschen1116@users.noreply.github.com>
2024-10-01 14:32:19 +0800

    fix(layer): correct shape of input and output

--- src/perceptron/network/Layer.py
+++ src/perceptron/network/Layer.py
@@ -17,13 +17,28 @@ class Linear(Module):
 
 		self.dtype: type = dtype
 
-	def forward(self, x: numpy.ndarray) -> numpy.ndarray:
+	def __concatenate_bias(self, x: numpy.ndarray) -> numpy.ndarray:
 		batch_size: int = x.shape[0]
-		bias: numpy.ndarray = numpy.ones((batch_size, 1), dtype=self.dtype)
+		bias: numpy.ndarray = numpy.ones((batch_size, 1, 1), dtype=self.dtype) * -1
 
-		x_with_bias: numpy.ndarray = numpy.concatenate((x, bias), axis=1, dtype=self.dtype)
+		return numpy.concatenate((x, bias), axis=-1, dtype=self.dtype)
 
-		return x_with_bias.dot(self.weights)
+	def forward(self, x: numpy.ndarray) -> numpy.ndarray:
+		x_i: numpy.ndarray = x
+		if len(x_i.shape) < 3:
+			raise Warning(f"x should be a batched 2d array, you input a {x.shape[0]} size batch of 1d array.")
+			x_i = numpy.expand_dims(x_i, 1)
+
+		self.__gradient: numpy.ndarray = numpy.empty(0)
+
+		return self.__concatenate_bias(x_i).dot(self.weights)
 
 	def backward(self, x: numpy.ndarray) -> numpy.ndarray:
-		return x
+		x_i: numpy.ndarray = x
+		if len(x_i.shape) < 3:
+			raise Warning(f"x should be a batched 2d array, you input a {x.shape[0]} size batch of 1d array.")
+			x_i = numpy.expand_dims(x_i, 1)
+
+		batch_size: int = x_i.shape[0]
+
+		return self.__concatenate_bias(x_i).sum(axis=0) / batch_size
