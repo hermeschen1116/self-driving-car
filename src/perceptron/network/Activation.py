e4c0d143806371cc18bcd6f5f408a02b08a011b8
hermeschen1116 <108386417+hermeschen1116@users.noreply.github.com>
2024-10-01 14:50:24 +0800

    feat(activation): implement sigmoid

--- src/perceptron/network/Activation.py
+++ src/perceptron/network/Activation.py
@@ -26,3 +26,28 @@ class ReLU(Module):
 		batch_size: int = x.shape[0]
 
 		return numpy.astype(x_i >= 0, x_i.dtype).sum(axis=0) / batch_size
+
+
+class Sigmoid(Module):
+	def __init__(self) -> None:
+		super(Sigmoid, self).__init__()
+
+	def forward(self, x: numpy.ndarray) -> numpy.ndarray:
+		self.__gradient: numpy.ndarray = numpy.empty(0)
+
+		x_i: numpy.ndarray = x
+		if len(x_i.shape) < 3:
+			print(f"Warning: x should be a batched 2d array, you input a {x.shape[0]} size batch of 1d array.")
+			x_i = numpy.expand_dims(x_i, 1)
+
+		return 1 / (1 + numpy.exp(x_i * -1))
+
+	def backward(self, x: numpy.ndarray) -> numpy.ndarray:
+		x_i: numpy.ndarray = x
+		if len(x_i.shape) < 3:
+			print(f"Warning: x should be a batched 2d array, you input a {x.shape[0]} size batch of 1d array.")
+			x_i = numpy.expand_dims(x_i, 1)
+
+		batch_size: int = x_i.shape[0]
+
+		return numpy.sum((1 / (1 + numpy.exp(x_i * -1))) * (1 - 1 / (1 + numpy.exp(x_i * -1))), axis=0) / batch_size
