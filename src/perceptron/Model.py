1ded23b6ac22d8727f2700ed87edbde04ab4a56e
hermeschen1116 <108386417+hermeschen1116@users.noreply.github.com>
2024-10-05 09:57:45 +0800

    fix(model): use previous method to set hidden layer

--- src/perceptron/Model.py
+++ src/perceptron/Model.py
@@ -18,7 +18,7 @@ class Perceptron(Module):
 	) -> None:
 		super(Perceptron, self).__init__()
 
-		hidden_layer_features: int = random.choice([i for i in range(*sorted([input_features, output_features]))])
+		hidden_layer_features: int = int((input_features + output_features) / 2)
 
 		self.input_layer = Linear(input_features, hidden_layer_features, dtype)
 		self.activation0 = ReLU()
