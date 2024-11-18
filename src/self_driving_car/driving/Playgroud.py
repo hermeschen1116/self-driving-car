dd2c81cb8a951e2a0ae5da1166417c19ae432949
hermeschen1116 <108386417+hermeschen1116@users.noreply.github.com>
2024-11-18 23:36:06 +0800

    refactor: remove debug code

--- src/self_driving_car/driving/Playgroud.py
+++ src/self_driving_car/driving/Playgroud.py
@@ -1,4 +1,4 @@
-from typing import List, Tuple
+from typing import List
 
 import numpy
 from matplotlib.lines import Line2D
@@ -13,23 +13,12 @@ class Playgroud:
 
 		self.edges: List[LineSegment] = []
 
-		print(goal[0].coordinate, goal[1].coordinate)
 		self.goal: LineSegment = LineSegment(goal[0], goal[1])
 
 		points = points[:-1]
 		num_points: int = len(points)
 		for i in range(num_points):
 			self.edges.append(LineSegment(points[i], points[(i + 1) % num_points]))
-
-	def playground_range(self) -> Tuple[float, float, float, float]:
-		x: List[float] = []
-		for edge in self.edges:
-			x += [edge.endpoint1.x, edge.endpoint2.x]
-		y: List[float] = []
-		for edge in self.edges:
-			y += [edge.endpoint1.y, edge.endpoint2.y]
-
-		return min(x), max(x), min(y), max(y)
 
 	def draw(self) -> List[Line2D]:
 		lines: List[Line2D] = []
